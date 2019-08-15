eedb1c98e5f40a80748e78cb7818fee54cd657f5
brucemolinanyc <brucemolinanyc@gmail.com>
2019-08-15 11:37:05 -0400

    Updates

---
+++ precache-manifest.6b68a7e29ee9811e9955aa9cfa76226c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d69cb4bfe6ef2a64cc09c0a821aaefb",
+    "url": "/monsters_roladex/index.html"
+  },
+  {
+    "revision": "eda61c215fba59fce6be",
+    "url": "/monsters_roladex/static/css/main.2b400823.chunk.css"
+  },
+  {
+    "revision": "336932d4be652fbae563",
+    "url": "/monsters_roladex/static/js/2.15967acd.chunk.js"
+  },
+  {
+    "revision": "eda61c215fba59fce6be",
+    "url": "/monsters_roladex/static/js/main.2f998acd.chunk.js"
+  },
+  {
+    "revision": "54be7a6ee80b61311e33",
+    "url": "/monsters_roladex/static/js/runtime~main.725f8c41.js"
+  }
+]);
